514c06a68ef0f5eb5f8a11a869da67154d27cc91
lefthandmedia <bob@lefthandmedia.com>
2014-08-27 11:28:09 +0200

    1e commit from Brecker

---
+++ submit.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Lefthandmedia <ralph@lefthandmedia.com>
+ */
+ 
+
+$dat = [['url'=>'/pictures/Biogewas.jpg','name'=>'plaatje1'],
+    ['url'=>'/pictures/Biologischrestaurant.jpg','name'=>'plaatje2'],
+    ['url'=>'/pictures/Biologischrestaurant1.jpg','name'=>'plaatje3'],
+    ['url'=>'/pictures/Biologischrestaurant2.jpg','name'=>'plaatje4']];
+
+echo json_encode($dat);
+
---
+++ vote.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @author Lefthandmedia <ralph@lefthandmedia.com>
+ */
+ 
+ 
